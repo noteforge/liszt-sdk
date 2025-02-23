abec44df870fa777a38bab10a91e12e41a0f4bf9
Kyle Vanderburg <kyle@noteforge.com>
2025-02-23 15:22:13 +0000

    CDN Updates

--- externals.php
+++ externals.php
@@ -1,32 +1,32 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 	<?php /*if(!$light){ ?>
-		<script src="//cdn.ntfg.net/vendor/moment/2.29.4/moment-with-locales.js"></script>
+		<script src="//cdn.liszt.app/vendor/moment/2.29.4/moment-with-locales.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/tom-select/dist/js/tom-select.complete.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
-		<!----><script src="//cdn.ntfg.net/vendor/colorpicker/3.2.0/js/bootstrap-colorpicker.min.js"></script><!---->
+		<!----><script src="//cdn.liszt.app/vendor/colorpicker/3.2.0/js/bootstrap-colorpicker.min.js"></script><!---->
 		<script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
-		<script src="//cdn.ntfg.net/vendor/bootpag/1.0.7/bootpag.min.js"></script>
+		<script src="//cdn.liszt.app/vendor/bootpag/1.0.7/bootpag.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
-		<script src="//cdn.ntfg.net/vendor/cleave.js/1.6.0/cleave.min.js"></script>
+		<script src="//cdn.liszt.app/vendor/cleave.js/1.6.0/cleave.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
 	<?php }*/ ?>	
-	<script src="//cdn.ntfg.net/vendor/iframe-resizer/4.3.6/js/iframeResizer.min.js"></script>
-	<script src="//cdn.ntfg.net/vendor/iframe-resizer/4.3.6/js/iframeResizer.contentWindow.min.js"></script>
+	<script src="//cdn.liszt.app/vendor/iframe-resizer/4.3.6/js/iframeResizer.min.js"></script>
+	<script src="//cdn.liszt.app/vendor/iframe-resizer/4.3.6/js/iframeResizer.contentWindow.min.js"></script>
 	<script src="https://kit.fontawesome.com/0bf7c0cd52.js" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel="stylesheet" href="//cdn.ntfg.net/vendor/bootstrap-milestones/1.0.4/dist/bootstrap-milestones.min.css" type="text/css" />
-	<link rel="stylesheet" href="//cdn.ntfg.net/common/fonts.css" type="text/css" />
+	<link rel="stylesheet" href="//cdn.liszt.app/vendor/bootstrap-milestones/1.0.4/dist/bootstrap-milestones.min.css" type="text/css" />
+	<link rel="stylesheet" href="//cdn.liszt.app/common/fonts.css" type="text/css" />
 	<?php /*if(!$light){ ?>
-		<link rel="stylesheet" href="//cdn.ntfg.net/vendor/trumbowyg/2.27.3/dist/ui/trumbowyg.min.css">
+		<link rel="stylesheet" href="//cdn.liszt.app/vendor/trumbowyg/2.27.3/dist/ui/trumbowyg.min.css">
 		<link href="https://cdn.jsdelivr.net/npm/tom-select/dist/css/tom-select.css" rel="stylesheet">
-		<link rel="stylesheet" href="//cdn.ntfg.net/vendor/colorpicker/3.2.0/css/bootstrap-colorpicker.min.css">
+		<link rel="stylesheet" href="//cdn.liszt.app/vendor/colorpicker/3.2.0/css/bootstrap-colorpicker.min.css">
 		<link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
-		<link rel="stylesheet" href="//cdn.ntfg.net/vendor/pace/1.2.7/minimal.css">
-		<link rel="stylesheet" href="//cdn.ntfg.net/vendor/overlayscrollbars/1.12.0/css/OverlayScrollbars.min.css">
-		<link rel="stylesheet" href="//cdn.ntfg.net/vendor/overlayscrollbars/1.12.0/css/os-theme-thin-dark.css">
+		<link rel="stylesheet" href="//cdn.liszt.app/vendor/pace/1.2.7/minimal.css">
+		<link rel="stylesheet" href="//cdn.liszt.app/vendor/overlayscrollbars/1.12.0/css/OverlayScrollbars.min.css">
+		<link rel="stylesheet" href="//cdn.liszt.app/vendor/overlayscrollbars/1.12.0/css/os-theme-thin-dark.css">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" type="text/css" media="all" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
 	<?php }*/ ?>
-	<link rel="stylesheet" href="//cdn.ntfg.net/common/nfcore.css">
-	<link rel="stylesheet" href="//cdn.ntfg.net/footer/footer.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="//cdn.liszt.app/common/nfcore.css">
+	<link rel="stylesheet" href="//cdn.liszt.app/footer/footer.css" type="text/css" media="all" />
